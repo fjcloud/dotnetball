3ac71a359548723b157e844fc4d497dc016fb0d9
Florian Jacquin <fj@msl.cloud>
2024-12-09 14:48:39 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseDefaultFiles();
 app.UseStaticFiles();
+app.UseDefaultFiles();
 
-app.MapGet("/api/ask", () => {
-    string[] responses = {
-        "It is certain", "Without a doubt", "Yes definitely",
-        "Most likely", "Outlook good", "Ask again later",
-        "Cannot predict now", "Don't count on it", "Very doubtful"
-    };
-    return Results.Json(new { answer = responses[Random.Shared.Next(responses.Length)] });
-});
+string[] responses = {
+    "It is certain", "Without a doubt", "Yes definitely",
+    "Most likely", "Outlook good", "Ask again later",
+    "Cannot predict now", "Don't count on it", "Very doubtful"
+};
+
+app.MapGet("/api/ask", () => 
+    TypedResults.Json(new { answer = responses[Random.Shared.Next(responses.Length)] }));
 
 app.Run();
